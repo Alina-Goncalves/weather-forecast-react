3ce73a36e6412aa0191d22f1996d255c72f73925
Alina-Goncalves <67426488+Alina-Goncalves@users.noreply.github.com>
2020-09-21 18:17:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Search from "./Search";
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <div className="App">
         <div className="container">
           <div className="appName">Weather Forecast</div>
+          <Search defaultCity="Porto" />
         </div>
       </div>
     </div>
