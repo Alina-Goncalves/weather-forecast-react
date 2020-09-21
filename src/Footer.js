878b14638b06a34f8e5e16c7f487800bd73cc3a1
Alina-Goncalves <67426488+Alina-Goncalves@users.noreply.github.com>
2020-09-21 19:17:38 +0100

    Added new component

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <br />
+      <br />
+      <a
+        rel="noopener noreferrer"
+        href="https://github.com/Alina-Goncalves/weather-forecast-react"
+        target="_blank"
+      >
+        Open-Source Code {""}
+      </a>
+      by Alina Gonçalves
+    </div>
+  );
+}
