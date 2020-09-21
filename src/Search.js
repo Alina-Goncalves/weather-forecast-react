f224f07fbe61d343df69ed6eb6f0d9904b3a1f57
Alina-Goncalves <67426488+Alina-Goncalves@users.noreply.github.com>
2020-09-21 18:48:40 +0100

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -4,6 +4,7 @@ import pushpin from "./images/pushpin.png";
 import bigEmoji from "./images/sun-big-cloud.png";
 import axios from "axios";
 import CurrentCityTemp from "./CurrentCityTemp";
+import Info from "./Info";
 
 export default function Search({ defaultCity }) {
   const [city, setCity] = useState(defaultCity);
@@ -68,6 +69,7 @@ export default function Search({ defaultCity }) {
         <div className="bigWeatherSymbol">
           <img src={bigEmoji} alt="Sun" />
         </div>
+        <Info data={response} />
       </div>
     );
   } else {
