7342cf93382f7d21fa2dc61b56fc6ce6c29dd61f
Alina-Goncalves <67426488+Alina-Goncalves@users.noreply.github.com>
2020-10-04 00:05:31 +0100

    Added new component

---
+++ src/ConvertUnit.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./ConvertUnit.css";
+
+export default function ConvertUnit({ celsius, dayMaxTemp, dayMinTemp}) {
+  const [unit, setUnit] = useState(celsius);
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit(celsius);
+  }
+
+  function fahrenheit() {
+    return (celsius * 9) / 5 + 32;
+  }
+  if (unit === celsius) {
+    return (
+      <div className="ConvertUnit">
+        <h2>{Math.round(celsius)}</h2>
+        <div className="units">
+          <a href="#/" className="degrees active">
+            °C
+          </a>
+          <span className="bar">|</span>
+          <a href="#/" className="fahrenheit" onClick={showFahrenheit}>
+            °F
+          </a>
+        </div>
+        <div className="dayTemperature" width="100px">
+          <span className="dayMaxTemperature">
+            {Math.round(dayMaxTemp)}°
+          </span>
+          <span className="dayMinTemperature">
+            {" "}
+            / {Math.round(dayMinTemp)}°
+          </span>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>{Math.round([fahrenheit()])}</h2>
+        <div className="units">
+          <a href="#/" className="degrees" onClick={showCelsius}>
+            °C
+          </a>
+          <span className="bar">|</span>
+          <a href="#/" className="fahrenheit active">
+            °F
+          </a>
+        </div>
+        <div className="dayTemperature" width="100px">
+          <span className="dayMaxTemperature">
+            {Math.round((dayMaxTemp * 9) / 5 + 32)}°
+          </span>
+          <span className="dayMinTemperature">
+            {" "}
+            / {Math.round((dayMinTemp * 9) / 5 + 32)}°
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
